40d51cdc32b728961e4fadd674b3eb203f1aca88
DanielAGWahby <danielashrafgerges@gmail.com>
2021-12-17 11:03:11 +0200

    Initial Commit

---
+++ Bosta_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bosta_Task
+//
+//  Created by Daniel Wahby on 17/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
